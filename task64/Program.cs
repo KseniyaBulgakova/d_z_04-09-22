28b917d0b1abe0f611781c537871e345c898e8f6
Kseniya <bulgakova.ks18@yandex.ru>
2022-09-04 15:27:29 +0300

    Спасибо за новые знания

---
+++ task64/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.Clear();
+int n = InputNumbers("Введите n: ");
+int count = 2;
+PrintNumbers (n, count);
+Console.Write("1 ");
+void PrintNumbers(int number, int count)
+{
+    if (count>n)
+    {
+        return;
+    }
+    PrintNumbers(n, count+1);
+    Console.Write( count + " ,");
+    }
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
---
+++ task66/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу,
+//  которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+Console.Clear();
+Console.WriteLine("Введите первое число :");
+int M = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число :");
+int N = int.Parse(Console.ReadLine());
+
+void GapNumberSum (int M, int N, int sum)
+{
+    if (M > N) 
+    {
+        Console.WriteLine($"Сумма чисел: {sum}"); 
+        return;
+    }
+    sum = sum + (M++);
+    GapNumberSum(M, N, sum);
+}
+
+GapNumberSum(M, N, 0);
